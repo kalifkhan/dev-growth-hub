6b0f31367845b6ce3ebefe2d875f0ad1a60e6a12
kalifkhan <kalifkhan1998@gmail.com>
2025-07-05 19:56:54 +0530

    updating

--- src/component/DragDrop.jsx
+++ src/component/DragDrop.jsx
@@ -1,10 +1,45 @@
-import React from 'react'
+import React from "react";
 
 const ITEMS = ["item1", "item2", "item3", "item4"];
 const DragDrop = () => {
-  return (
-    <div>DragDrop</div>
-  )
-}
+  const handleDragStart = (index) => {
+    console.log("Item dragged:", index);
+  };
 
-export default DragDrop
+  const handleDrop = () => {
+    console.log("Item dropped");
+  };
+  return (
+    <div style={{ width: "400px", background: "lightBlue", padding: "10px" }}>
+      DragDrop
+      {ITEMS
+        ? ITEMS?.map((item) => (
+            <div
+            key={item}
+              style={{
+                border: "1px solid black",
+                marginBottom: "10px",
+                background: "grey",
+                cursor: "pointer",
+                padding: "10px",
+              }}
+              draggable
+              onDragStart={() => {
+                handleDragStart(item);
+              }}
+              // onDragOver={(e) => {
+              //   e.preventDefault();
+              // }}
+              onDrop={() => {
+                handleDrop(item);
+              }}
+            >
+              {item}{" "}
+            </div>
+          ))
+        : null}
+    </div>
+  );
+};
+
+export default DragDrop;
